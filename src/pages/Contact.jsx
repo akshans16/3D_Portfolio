9ef30eca0affefb912eb0b61ef5abb337bf4ffaa
Akshansh Khare <akshanskhare@gmail.com>
2025-10-21 18:35:43 +0530

    create contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,145 @@
-import React from 'react'
-
+import {useState, useRef} from "react";
+import emailjs from "@emailjs/browser";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Fox from '../models/Fox'
+import  Loader  from '../components/Loader';
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+    const formRef = useRef(null);
+    const [form, setform] = useState({name: "", email: "", message: ""});
 
-export default Contact
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+    const handleChange = (e) => {
+        setform({...form, [e.target.name]: e.target.value});
+    };
+    const handleFocus = (e) => {
+      setCurrentAnimation('walk');
+
+    };
+    const handleBlur = () => {
+      setCurrentAnimation('idle');
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setCurrentAnimation('hit');
+        emailjs
+        .send(
+            import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+
+            import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+            {
+                from_name: form.name,
+                to_name: "Akshansh",
+                from_email: form.email,
+                to_email: "youmarcos7974@gmail.com",
+                message: form.message,
+            },
+            import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then(() => {
+            setIsLoading(false);
+            // TODO: Show success message
+            // TODO: Hide an alert
+            setform({name: "", email: "", message: ""});
+            setTimeout(() => {
+              setCurrentAnimation('idle')
+               setform({name: "", email: "", message: ""});
+            }, 3000);
+        })
+        .catch((error) => {
+          setCurrentAnimation('idle');
+            setIsLoading(false);
+            console.log(error);
+            // TODO: Show error message
+        });
+
+    };
+
+    return (
+        <section className="relative flex lg:flex-row flex-col max-container">
+            <div className="flex-1 min-w-[50%] flex flex-col">
+                <h1 className="head-text">Get in touch</h1>
+                <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+                    <label className="text-black-500 font-semibold">
+                        Name
+                        <input
+                            type="text"
+                            name="name"
+                            className="input"
+                            placeholder="John"
+                            required
+                            value={form.name}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+
+                    <label className="text-black-500 font-semibold">
+                        Email
+                        <input
+                            type="email"
+                            name="email"
+                            className="input"
+                            placeholder="johndoe@gmail.com"
+                            required
+                            value={form.email}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+
+                    <label className="text-black-500 font-semibold">
+                        Your Message
+                        <textarea
+                            name="message"
+                            rows={4}
+                            className="textarea"
+                            placeholder="Let me know how I can help you!"
+                            required
+                            value={form.message}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+
+                    <button
+                        type="submit"
+                        className="btn"
+                        disabled={isLoading}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    >
+                        {isLoading ? "Sending..." : "Send Message "}
+                    </button>
+                </form>
+            </div>
+
+            <div className="lg:w-1/2 ml-10 w-full lg:h-auto md:h-[550px] h-[350px]">
+                <Canvas camera={{position: [0, 0, 5],
+                  fov:75,
+                  near:0.1,
+                  far:1000
+                }}>
+                  <ambientLight intensity={0.5}/>
+                  <directionalLight intensity={2.5} position={[0,0,1]}/>
+                  <Suspense fallback = {<Loader/>}>
+                    <Fox
+                    currentAnimation={currentAnimation}
+                    position = {[0.5,0.35,0]}
+                    rotation = {[12.6,-0.625,0]}
+                    scale = {[0.6,0.6,0.6]}/>
+                  </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
